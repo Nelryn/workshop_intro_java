8870dc745f5bb1dd4b0eae03b4f8c6e5131d1636
Jojolions <emilie.baunifais@epitech.eu>
2023-01-30 17:20:49 +0400

    ex 4

--- WorkshopJava/src/main/java/com/github/Nelryn/workshop/Fibonacci.java
+++ WorkshopJava/src/main/java/com/github/Nelryn/workshop/Fibonacci.java
@@ -1,16 +1,21 @@
 package com.github.Nelryn.workshop;
 
 public class Fibonacci {
-    public static int nb1 = 10;
-    public static int nb2 = 10;
+    public static int nb1 = 1;
+    public static int nb2 = 1;
+    public static int max = 10;
+
     public static void main(String[] args) {
-        if (args.length == 1) {
-            nb1 = Integer.parseInt(args[0]);
-            nb2 = nb1;
-        }
+        if (args.length == 1)
+            max = Integer.parseInt(args[0]);
     }
 
     public static void computeIt() {
-        
+        int swap = nb2;
+        for (int i = 1; i < max; i++) {
+            swap = nb2;+2
+            nb2 = nb1 + nb2;
+            nb1 = swap;
+        }
     }
 }
